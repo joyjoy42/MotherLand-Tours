703560d799eba5b2f8f95fcd13f046974aaed737
joyjoy42 <akodekoumerveillejoy@gmail.com>
2026-03-11 20:14:57 +0000

    Enhance Vercel diagnostics with bridge fix and phpinfo

--- api/index.php
+++ api/index.php
@@ -1,2 +1,11 @@
 <?php
-require __DIR__ . '/../public/index.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$publicIndex = __DIR__ . '/../public/index.php';
+
+if (!file_exists($publicIndex)) {
+    die("Error: public/index.php not found at " . $publicIndex);
+}
+
+require $publicIndex;
