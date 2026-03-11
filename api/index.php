7748d1d3b5cb082691339968dfbe6819074c1782
joyjoy42 <akodekoumerveillejoy@gmail.com>
2026-03-11 20:34:10 +0000

    Add APP_KEY guard to Vercel bridge

--- api/index.php
+++ api/index.php
@@ -3,5 +3,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // This file is the entry point for Vercel.
+if (!getenv('APP_KEY')) {
+    die("Error: APP_KEY is not set in Vercel Environment Variables.");
+}
+
 // We proxy the request to the Laravel application.
 require __DIR__ . '/../public/index.php';
